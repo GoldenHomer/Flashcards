f2bc7de8566e499702a16f687cdd8fc85aa32ef3
Jose Contreras <jocontre@joselcontreras.com>
2014-07-31 21:39:30 -0700

    Controllers, routes and methods

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -17,4 +17,14 @@ class DecksController < ApplicationController
 	def show
 		@deck = Deck.find(params["id"])
 	end
+
+	def new
+		@deck = Deck.new
+	end
+
+	def create
+		@deck = Deck.new(params["deck"])
+		@deck.save
+		redirect_to "/decks"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Flashcards::Application.routes.draw do
   get "/decks" => "decks#index"
                     #ControllerName#ActionMethod 
                     #This is how Ruby parses this
+  get "decks/new" => "decks#new"
   get "/decks/:id" => "decks#show"
   # :id is interpolation for multiple id numbers!
+
+  #Also, routes in Rails are order specific from top to bottom.
+  post "/decks" => "decks#create"
 end
