655fa7c4db427caa5211e06c9f494dfbf8098a59
Jose Contreras <jocontre@joselcontreras.com>
2014-07-31 20:50:06 -0700

    Doing more with Rails

---
+++ app/controllers/decks_controller.rb
@@ -0,0 +1,20 @@
+class DecksController < ApplicationController
+	def index
+		# Actions are methods that are used by routes
+		@decks = Deck.all
+		# Deck is the model name. If you have an instance variable in controller, you have one in view as well.
+	end
+# Strong parameters that replaces the deprecated attr_accessible (video is outdated)
+	# def create
+	# 	@deck = Deck.new(deck_params)
+	# end
+
+	# private
+	# def deck_params
+	# 	params.require(:deck).permit(:name)
+	# end
+
+	def show
+		@deck = Deck.find(params["id"])
+	end
+end
